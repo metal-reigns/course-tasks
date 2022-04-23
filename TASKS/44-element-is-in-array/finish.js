83c65aa55f881275997d02f86dd236d913914199
metal-reigns <isniper19931221@yahoo.com>
2022-04-23 20:02:49 +0300

    includesArray

--- TASKS/44-element-is-in-array/finish.js
+++ TASKS/44-element-is-in-array/finish.js
@@ -7,8 +7,14 @@
  * 3. В противном случае вернуть "false"
  */
 
+const isElementInArray = (inputArray, searchElement) => {
+    return inputArray.includes(searchElement)
+    
+}
+
 const transports = ['Bus', 'Car', 'Bicycle', 'Airplane']
 
 console.log(isElementInArray(transports, 'Bus')) // true
 console.log(isElementInArray(transports, 'Phone')) // false
 console.log(isElementInArray(transports, 'Airplane')) // true
+
